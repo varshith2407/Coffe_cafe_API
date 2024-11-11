acfca19e66062664631489d4a7edc3fafd073fcd
varshith goud <varshithgoud99@gmail.com>
2024-11-11 15:56:33 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,12 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+engine=create_engine("sqlite:///./init.db",
+    echo=True
+)
+
+Base=declarative_base()
+
+Session=sessionmaker()
